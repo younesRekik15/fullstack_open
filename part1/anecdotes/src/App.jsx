8824af9ebc5366319efbdd44647de4d41c2add5a
younesRekik15 <younesrekik347@gmail.com>
2026-07-28 14:01:19 +0100

    anecdotes step 2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,11 +13,20 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
-
+  const [votes, SetVotes] = useState({0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0})
+  const getRandomNumber = () => Math.floor(Math.random()*anecdotes.length)
+  const voteHandler = ()=>{
+    const copy = { ...votes }
+    copy[selected]+=1
+    SetVotes(copy)
+  }
+console.log(votes)
   return (
     <div>
       <p>{anecdotes[selected]}</p>
-      <button onClick={()=>{setSelected(Math.floor(Math.random()*anecdotes.length))}}>next anecdote</button>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={voteHandler}>vote</button>
+      <button onClick={()=>{setSelected(getRandomNumber())}}>next anecdote</button>
     </div>
   )
 }
