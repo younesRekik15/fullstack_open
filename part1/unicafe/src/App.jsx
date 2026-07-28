3ab5e64e2dcb135aa3e9fad2f54d1d76e6a01e9e
younesRekik15 <younesrekik347@gmail.com>
2026-07-28 12:02:48 +0100

    unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,7 +9,10 @@ const Button = ({onClick, text}) =>{
 const StatisticLine = ({text, value}) =>{
   
       return (
-        <p>{text} {value}{text=='positive'?'%':''}</p>
+        <tr>
+          <td>{text}</td>
+          <td>{value}{text=='positive'?'%':''}</td>
+        </tr>
       )
   
 }
@@ -21,14 +24,16 @@ const Statistics = (props) => {
     )
   }else{
     return(
-      <>
-        <StatisticLine text='good' value = {props.good}/>
-        <StatisticLine text='neutral' value = {props.neutral}/>
-        <StatisticLine text='bad' value = {props.bad}/>
-        <StatisticLine text='all' value = {props.all} />
-        <StatisticLine text='average' value = { props.average } />
-        <StatisticLine text = 'positive' value = {props.positive} />    
-      </>
+      <table>
+        <tbody>
+          <StatisticLine text='good' value = {props.good}/>
+          <StatisticLine text='neutral' value = {props.neutral}/>
+          <StatisticLine text='bad' value = {props.bad}/>
+          <StatisticLine text='all' value = {props.all} />
+          <StatisticLine text='average' value = { props.average } />
+          <StatisticLine text = 'positive' value = {props.positive} />   
+        </tbody> 
+      </table>
     )
   }
 }
