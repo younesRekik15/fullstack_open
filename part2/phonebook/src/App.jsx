7e47bcc52784ec9b638f33b8efcbae894679051a
younesRekik15 <younesrekik347@gmail.com>
2026-07-31 18:09:13 +0100

    the phonebook step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({value, onChange}) => {
   return(
@@ -37,12 +38,15 @@ const Persons = ({searchValue, persons}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+  useEffect(()=>{
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response =>{
+        setPersons(response.data)
+      })
+  }
+  ,[])
   const  [newName, setNewName] = useState('')
   const  [newNumber, setNewNumber] = useState('')
   const  [searchValue, setSearchValue] = useState('')
